2ae8110c96fe0d2ee9cb823ba87144d003f04853
Gustav X <sillygorilla42@gmail.com>
2025-12-04 05:53:25 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
+scene.setBackgroundColor(12)
 tiles.setCurrentTilemap(tilemap`level1`)
 let mySprite = sprites.create(img`
-    ..ccc.........fffffff...
-    ..f4cc.......ffcc22ff...
-    ..f44cc...fffccccfff....
-    ..f244cccc22224442cc....
-    ..f224cc2222222244b9c...
-    ..cf2222222222222b999c..
-    .c22c222222222b11199b2c.
-    f22ccccccc222299111b222c
-    fffffcc222c222222222222f
-    .....f2222442222222222f.
-    ....f222244fc2222222ff..
-    ...c222244ffffffffff....
-    ...c2222cfffc2f.........
-    ...ffffffff2ccf.........
-    .......ffff2cf..........
-    ........fffff...........
+    . . . . . . . . . . . . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 . . . . . 
+    . 2 2 2 2 2 2 2 2 2 2 2 . . . . 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    2 . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+    2 . . 1 1 1 1 1 1 1 1 1 2 2 2 . 
+    1 . 1 1 1 1 1 1 1 e e e 1 1 1 . 
+    . . 1 1 e 4 4 e b f 4 4 e e f . 
+    . . 1 e e 4 d 4 1 f d d e f . . 
+    . . . f e e e e e d d d f . . . 
+    . . . . . f 4 d d e 4 e f . . . 
+    . . . . . f e d d e 2 2 f . . . 
+    . . . . f f f e e f 5 5 f f . . 
+    . . . . f f f f f f f f f f . . 
+    . . . . . f f . . . f f f . . . 
     `, SpriteKind.Player)
+controller.moveSprite(mySprite)
